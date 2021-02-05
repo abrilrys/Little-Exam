c672ec5122cd4d930da719210923da2e6b9d26d4
Abril <abrilseyer12@gmail.com>
2021-02-05 16:37:59 -0600

    first commit

---
+++ Assets/GameContol.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour
+{
+
+    [SerializeField]
+    public GameObject gameOverText;
+    public Text scoreText;
+    public static GameControl instance;
+    public bool gameOver = false;
+    
+    public float scrollSpeed = -1.5f;
+
+    private int score = 0;
+    private int scoredeaths= 0;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameOver == true  && Input.GetKeyDown(KeyCode.Space))
+        { 
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+
+    public void Scored()
+    {
+        if (gameOver)
+        {
+            enabled = false;
+            SceneManager.LoadScene("SampleScene");
+
+        }
+        score++;
+        scoreText.text = "Wins:  " + score.ToString();
+    }
+
+    public void Died()
+    {
+        gameOver = true;
+
+        if (gameOver == true)
+        {
+            gameOverText.SetActive(true);
+        }
+        scoredeaths++;
+    }
+
+    
+
+}
---
+++ Assets/Jump.cs
@@ -0,0 +1,52 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump: MonoBehaviour
+{
+    public float upForce = 500f;
+    public int player_num;
+    public float speed=4;
+    private bool isDead = false;
+    private Rigidbody2D rb2d;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDead == false)
+        {
+            rb2d.AddForce(new Vector2(speed, 0));
+            if (Input.GetKeyDown(KeyCode.Space) && player_num == 0) 
+            {
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(speed, upForce));
+            }
+        }
+        
+    }
+
+    void OnCollisionEnter2D()
+    {
+       
+
+        if (isDead == false && player_num == 0)
+        {
+            rb2d.velocity = Vector2.zero;
+            isDead = true;
+            GameControl.instance.Died();
+        }
+
+      
+    }
+
+}
+
